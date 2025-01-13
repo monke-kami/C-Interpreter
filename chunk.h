7d88d4864b60c709d2d42d69d4fd71b14524b150
d1 <devanbmohan@gmail.com>
2025-01-13 19:31:35 +0530

    added dynamic memory alloc

---
+++ chunk.h
@@ -0,0 +1,17 @@
+#ifndef clox_chunk_h
+#define clox_chunk_h
+
+#include "headerstuff.h"
+
+typedef enum {
+	OP_RETURN,
+} Opcode; //for instructions
+
+typedef struct {
+	int count;
+	int capacity;
+	unit8_t* code;
+} Chunk; //for storing other data
+
+void initChunk(Chunk* chunk);
+#endif
