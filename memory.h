83163bab34cdba1f5991fcf8c9f4da0d19ae785f
d1 <devanbmohan@gmail.com>
2025-01-13 21:34:15 +0530

    memory.h has been created

---
+++ memory.h
@@ -0,0 +1,12 @@
+#ifndef clox_memory_h
+#define clox_memory_h
+
+#include "common.h"
+
+#define GROW_CAPACITY(capacity) \
+	((capacity) < 8 ? 8 : (capacity) * 2)
+
+#define GROW_ARRAY(type,pointer,oldcount,newCount) \
+	(type*)reallocate(pointer , sizeof(type) * (oldcount), \
+	sizeof(type) * (newCount))
+#endif
