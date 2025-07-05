b7e61a16573281166a224d29b5cc152d7493cae3
labal <devanbmohan@gmail.com>
2025-07-05 09:12:26 +0000

    started main from scratch again

--- main.c
+++ main.c
@@ -1,12 +1,5 @@
-#include "headerstuff.h"
-#include "chunk.h"
-#include "debug.h"
+#include "common.h"
 
-int main(int argc,const char* argv[]){
-	Chunk chunk;
-	initChunk(&chunk);
-	writeChunk(&chunk, OP_RETURN);
-	disassembleChunk(&chunk,"test chunk");
-	freeChunk(&chunk);
+int main(int argc, const char* argv[]){
 	return 0;
 }
