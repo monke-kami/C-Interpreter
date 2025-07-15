cdb550f151ae330d8c495efb9365674b3c3865c6
labal <devanbmohan@gmail.com>
2025-07-15 18:04:16 +0000

    added a instruction disassembler

--- debug.c
+++ debug.c
@@ -9,4 +9,19 @@ void disassembleChunk(Chunk* chunk, const char* name) {
     offset = disassembleInstruction(chunk, offset);
   }
 }
+static int simpleInstrutction(const char * name, int offset){
+  printf("%s\n",name);
+  return offset + 1;
+}
+int disassembleInstruction(Chunk* chunk, int offset){
+  printf("%04d ", offset);
+  uint8_t instruction = chunk->code[offset];
+  switch (instruction) {
+    case OP_RETURN:
+      return simpleInstruction("OP_RETURN", offset);
+    default:
+      printf("unknown opcode %d\n",instruction);
+      return offset + 1;
+  } 
+}
 
