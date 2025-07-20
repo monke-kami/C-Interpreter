ee765fa6f14aab656fc0c26bf94571fc3d4b6d87
labal <devanbmohan@gmail.com>
2025-07-20 18:08:19 +0000

    added three new func

---
+++ value.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+#include "memory.h"
+#include "value.h"
+
+void initValueArray(ValueArray* array) {
+    array->value = NULL;
+    array->capacity = 0; 
+    array->count = 0;
+}
+void writeValueArray(ValueArray* array,Value value){
+    if(array->capacity<array->count + 1){
+        int oldcapacity = array->capacity;
+        array->capacity = GROW_CAPACITY(oldcapacity);
+        array->values = GROW_ARRAY(Value, array->values,oldcapacity,array->capacity);
+
+    }
+    array->values[array->count] = value;
+    array->count++;
+}
+void freeValueArray(ValueArray* array) {
+  FREE_ARRAY(Value, array->values, array->capacity);
+  initValueArray(array);
+}
+
